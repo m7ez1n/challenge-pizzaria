5073a6edd107bafdfd409262316dabce2bebd6c5
m7Aei <mateuspm2015@hotmail.com>
2019-07-20 03:40:55 -0300

    Fazendo as configurações do mongoose

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
+  }
+
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
   middlewares () {
