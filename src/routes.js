86073f214d9ba11180c25ca3607e5e318dc97451
m7Aei <mateuspm2015@hotmail.com>
2019-07-21 01:07:21 -0300

    Acho que adicionei todas as rotas sla

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
 const PizzaController = require('./app/controllers/PizzaController')
 const PizzaValidator = require('./app/validators/Pizza')
+const OrderController = require('./app/controllers/OrderController')
+
+// CRUD Orders
+routes.get('/api/orders', OrderController.index)
+routes.get('/api/orders/:id', OrderController.show)
+routes.post('/api/orders', OrderController.store)
+routes.put('/api/orders/:id', OrderController.update)
+routes.delete('/api/orders/:id', OrderController.destroy)
+
+// Pizzas
+routes.get('/api/pizzas', validate(PizzaValidator), PizzaController.index)
 
 module.exports = routes
