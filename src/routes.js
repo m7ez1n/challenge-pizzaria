80969236392d2a9e7d304ef1805eeece4e370de1
m7Aei <mateuspm2015@hotmail.com>
2019-07-21 00:52:12 -0300

    Criando o controller e exportanto para as rotas

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,7 @@ const express = require('express')
 
 const routes = express.Router()
 
+const PizzaController = require('./app/controllers/PizzaController')
+const PizzaValidator = require('./app/validators/Pizza')
+
 module.exports = routes
