61fa4a1c891159179aa88972fb9717614ed7d4a2
m7Aei <mateuspm2015@hotmail.com>
2019-09-20 00:51:30 -0300

    Criando uma regra de negocios no controller

--- v2/src/app/controllers/PizzaController.js
+++ v2/src/app/controllers/PizzaController.js
@@ -2,6 +2,14 @@ import Pizza from '../models/Pizza';
 
 class PizzaController {
   async store(req, res) {
+    const { name } = req.body;
+
+    const checkName = await Pizza.findOne({ where: { name } });
+
+    if (checkName) {
+      return res.status(400).json({ error: 'Duplicated name' });
+    }
+
     const pizza = await Pizza.create(req.body);
 
     return res.json(pizza);
