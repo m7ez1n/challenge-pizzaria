37960db18b970856541d847fdae522ce64083c46
m7Aei <mateuspm2015@hotmail.com>
2019-09-20 00:50:57 -0300

    Fazendo o teste de nome duplicado

--- v2/__tests__/integration/pizza.test.js
+++ v2/__tests__/integration/pizza.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('Pizza', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able register one pizza', async () => {
     const response = await request(app)
       .post('/api/pizzas')
@@ -12,5 +18,25 @@ describe('Pizza', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated pizza name', async () => {
+    await request(app)
+      .post('/api/pizzas')
+      .send({
+        name: 'Bufala',
+        price: 29.9,
+        ingredients: 'tomato, mozarella di bufala',
+      });
+
+    const response = await request(app)
+      .post('/api/pizzas')
+      .send({
+        name: 'Bufala',
+        price: 29.9,
+        ingredients: 'tomato, mozarella di bufala',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
